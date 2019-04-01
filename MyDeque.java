537dfda230e94807a3d46b902d177ebefab68ecd
athompson00 <athompson00@stuy.edu>
2019-04-01 01:53:55 -0400

    created expand and addFirst methods

--- MyDeque.java
+++ MyDeque.java
@@ -5,7 +5,14 @@ public class MyDeque<E>{
   private int size, start, end;
 
   @SuppressWarnings("unchecked")
-  
+  public static void main(String[] args){
+    MyDeque<Integer> a = new MyDeque<Integer>(10);
+    a.addFirst(1);
+    System.out.println(a.toString());
+  }
+
+  @SuppressWarnings("unchecked")
+
   public MyDeque(){
     data = (E[])new Object[10];
     size = 0;
@@ -13,16 +20,85 @@ public class MyDeque<E>{
     end = 0;
   }
 
-  public MyDeque(int initialCapacity){
+  @SuppressWarnings("unchecked")
+public MyDeque(int initialCapacity){
     data = (E[])new Object[initialCapacity];
+}
+
+public int size(){
+  return size;
+}
+  public String toString(){
+    String result = "{";
+    if (size != 0){
+      int startler = start;
+      while (startler <= end){
+        if (!data[startler].equals(null)){
+          result += data[startler] + " ";
+        }
+        startler++;
+      }
+      while (startler != end + 1){
+        if (!data[startler].equals(null)){
+          result += data[startler] + " ";
+        }
+        if (startler == data.length - 1){
+          startler = 0;
+        } else {
+          startler++;
+        }
+      }
+    }
+    return result + "}";
   }
 
-  public int size(){ }
-  public String toString(){ }
-  public void addFirst(E element){ }
-  public void addLast(E element){ }
-  public E removeFirst(){ }
-  public E removeLast(){ }
-  public E getFirst(){ }
-  public E getLast(){ }
+  @SuppressWarnings("unchecked")
+  public void expand(){
+    E[] temp = (E[])new Object[data.length * 2 + 1];
+    int a = start;
+    for (int i = 0; i < data.length; i++){
+      temp[i] = data[a];
+      if (a == data.length - 1){
+        a = 0;
+      } else {
+        a++;
+      }
+    }
+    data = temp;
+  }
+
+  public void addFirst(E element){
+    if (size == 0){
+      data[start] = element;
+      size++;
+    } else {
+      if (start == 0){
+        start = data.length - 1;
+      } else if (size == data.length){
+        expand();
+        start = data.length - 1;
+        end = size - 1;
+      } else {
+        start--;
+      }
+      size++;
+      data[start] = element;
+    }
+  }
+  /*
+  public void addLast(E element){
+
+  }
+  public E removeFirst(){
+
+  }
+  public E removeLast(){
+
+  }
+  public E getFirst(){
+
+  }
+  public E getLast(){
+
+  }*/
 }
